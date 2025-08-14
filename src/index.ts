1cb23c8883a81864d702418c0fcb5c26b8db5b40
m4nu-git <manudev912@gmail.com>
2025-08-14 15:55:38 +0530

    initial code setup

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import { serverConfig } from "./config/config";
+import v1Router from "./routers/v1/index.router";
+import v2Router from "./routers/v2/index.router";
+
+const app = express();
+
+/**
+ * Registering all the routers and theirs corresponding routes with out app server object.
+ */
+app.use("/api/v1", v1Router);
+app.use("/api/v2", v2Router);
+
+app.listen(serverConfig.PORT, () => {
+  console.log(`Server is running on http://localhost:${serverConfig.PORT}`);
+  console.log(`Press Ctrl+C to stop the server.....`);
+});
